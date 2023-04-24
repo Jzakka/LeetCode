06b3e31809f79359c6c710c3e7dfaac377d8d59c
Jzakka <105845911+Jzakka@users.noreply.github.com>
2023-04-24 11:08:35 +0900

    Time: 8 ms (71.81%), Space: 8.4 MB (96.82%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        do{
+            result.push_back(nums);
+        } while (std::next_permutation(nums.begin(), nums.end()));
+
+        return result;
+    }
+};
