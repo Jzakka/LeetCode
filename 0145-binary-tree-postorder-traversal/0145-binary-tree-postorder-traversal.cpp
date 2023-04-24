bd853188cb419a903703b792ea22194e726cd48b
Jzakka <105845911+Jzakka@users.noreply.github.com>
2023-04-24 11:41:58 +0900

    Time: 3 ms (49.76%), Space: 8.4 MB (71.49%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if (root == nullptr) {
+            return {};
+        }
+        stack<TreeNode*> stk;
+        unordered_set<TreeNode*> visited;
+        stk.push(root);
+        vector<int> res;
+//        res.push_back(root->val);
+        while (!stk.empty()){
+            auto node = stk.top();
+
+            if (node->left != nullptr && visited.find(node->left) == visited.end()) {
+                stk.push(node->left);
+//                res.push_back(stk.top()->val);
+                visited.insert(node->left);
+            }
+            else if (node->right != nullptr && visited.find(node->right) == visited.end()) {
+                stk.push(node->right);
+//                res.push_back(stk.top()->val);
+                visited.insert(node->right);
+            }else{
+                res.push_back(node->val);
+                stk.pop();
+            }
+        }
+        return res;
+    }
+};
